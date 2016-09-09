b90a5b0d5c774bf35d0c859c1e75fb7741208b20
bsr <bsr@Babus-MacBook-Air.local>
2016-09-09 17:44:49 -0400

    working

--- src/__tests__/Foo.test.js
+++ src/__tests__/Foo.test.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 
-jest.dontMock('../Foo');
-
-const Foo = require('../Foo');
+import Foo from '../Foo';
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
-    //expect(shallow(<Foo />).contains(<div className="foo" />)).toBe(true);
+    expect(shallow(<Foo />).contains(<div className="foo" />)).toBe(true);
   });
 
   it("contains spec with an expectation", function() {
-    //expect(shallow(<Foo />).is('.foo')).toBe(true);
+    expect(shallow(<Foo />).is('.foo')).toBe(true);
   });
 
   it("contains spec with an expectation", function() {
-    //expect(mount(<Foo />).find('.foo').length).toBe(1);
+    expect(mount(<Foo />).find('.foo').length).toBe(1);
   });
 });
